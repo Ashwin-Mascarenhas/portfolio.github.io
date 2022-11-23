013088fc508296ab403f19858e11981944d0df2d
Ashwin-Mascarenhas <90493499+Ashwin-Mascarenhas@users.noreply.github.com>
2022-11-23 13:11:29 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+//get data from form  
+//phpinfo();
+echo("Hello world");
+$name = $_POST['name'];
+$email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+$to = "ashwinmascarenhas@outlook.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+if($email!=NULL){
+   mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
